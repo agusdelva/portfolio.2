b235a65dbe7c7b31a8bbcbe12369261facd21189
agustin del valle <agusdelva.16@gmail.com>
2021-02-07 15:11:15 -0300

    header responsive

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const button = document.getElementById('Home-button')
+// addEventListener('DOMContentLoader', () => {
+//     const navbar_btn = document.querySelector('.navbar-btn')
+//     if(navbar_btn){
+//         navbar_btn.addEventListener('click', () => {
+//             const navbar_items = document.querySelector('navbar-items')
+//             navbar_items.classList.toggle('show')
+//         })
+//     }
+// })
 
-
-
-button.addEventListener('click', {
-
-
-    }
-    )
+function showmenu() {
+    document.getElementById('otroModo').classList.toggle('show')
+}
