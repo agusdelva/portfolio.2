1a0a05ccb2fd4c376bf1a2052648c555b509f8ea
agustin del valle <agusdelva.16@gmail.com>
2021-05-22 17:00:15 -0300

    localStorage darkmode

--- main.js
+++ main.js
@@ -3,4 +3,21 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark')
     btnSwitch.classList.toggle('active')
-})
+
+    //guardamos en localStorage
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false')
+    }
+});
+
+//obtenemos el modo actual
+ if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark')
+    btnSwitch.classList.add('active')
+ } else {
+    document.body.classList.remove('dark')
+    btnSwitch.classList.remove('active')
+ }
+
